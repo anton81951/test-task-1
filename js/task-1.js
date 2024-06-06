7c75c6a1ab4fb6e52fd03ab547c37c398faf5029
Anton81951 <antonbugaichuk@gmail.com>
2024-06-06 11:53:07 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,48 @@
 'use strict';
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity*pricePerDroid} credits!`;
+function calculateTeamFinanceReport(salaries, team) {
+    const report = {
+        totalBudgetTeam: 0
+    };
+
+    // Initialize all specializations in the report with 0
+    for (let specialization in salaries) {
+        const prop = `totalBudget${specialization}`;
+        report[prop] = 0;
+    }
+
+    team.forEach(member => {
+        const specialization = member.specialization;
+        const salaryDetails = salaries[specialization];
+
+        if (salaryDetails) {
+            const salary = salaryDetails.salary;
+            const tax = parseFloat(salaryDetails.tax) / 100;
+            const netSalary = salary - (salary * tax);
+
+            report.totalBudgetTeam += netSalary;
+
+            const prop = `totalBudget${specialization}`;
+            report[prop] += netSalary;
+        }
+    });
+
+    report.totalBudgetTeam = Math.round(report.totalBudgetTeam);
+    for (let specialization in salaries) {
+        const prop = `totalBudget${specialization}`;
+        report[prop] = Math.round(report[prop]);
+    }
+
+    return report;
 }
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const salaries1 = {
+    Manager: { salary: 1000, tax: "10%" },
+    Designer: { salary: 600, tax: "30%" },
+    Artist: { salary: 1500, tax: "15%" },}
+ const team1 = [
+    { name: "Misha", specialization: "Manager" },
+    { name: "Max", specialization: "Designer" },
+    { name: "Vova", specialization: "Designer"},
+    { name: "Leo", specialization: "Artist"},]
+ const financeReport1 = calculateTeamFinanceReport(salaries1, team1)
+ console.log(JSON.stringify(financeReport1))
+
